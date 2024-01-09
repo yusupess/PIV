84a4a591b2eb5d4ac5e5c60c07028673734e4160
yusup <yusupessu@gmail.com>
2024-01-09 12:53:38 +0400

    работает таблица в админке на локалхосте

--- PIV/catalog/admin.py
+++ PIV/catalog/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Part
 
-class IngredientAdmin(admin.ModelAdmin):
+class PartAdmin(admin.ModelAdmin):
     list_display = (
         'part_number',
         'name',
     )
     search_fields = ('name', 'part_number')
     list_filter = ('name',)
+
+admin.site.register(Part, PartAdmin)
